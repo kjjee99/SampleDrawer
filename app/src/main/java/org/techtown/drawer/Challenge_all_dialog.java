334a9b29fc7c0f1f0075997715836fc3c084033b
kjjee99 <kjjee99@naver.com>
2021-09-24 15:04:35 +0900

    챌린지 여러 개 저장하기

--- app/src/main/java/org/techtown/drawer/Challenge_all_dialog.java
+++ app/src/main/java/org/techtown/drawer/Challenge_all_dialog.java
@@ -3,6 +3,8 @@ package org.techtown.drawer;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.ArraySet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +12,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class Challenge_all_dialog extends androidx.fragment.app.DialogFragment {
 
@@ -19,8 +24,11 @@ public class Challenge_all_dialog extends androidx.fragment.app.DialogFragment {
         super.onCreate(savedInstanceState);
     }
     //variable
-    SharedPreferences sharedPreferences;
+    SharedPreferences prefs, sharedPreferences;
     public static final String MyPreferences = "MyPrefs";
+    Set<String> titles = new ArraySet<>();
+    Set<String> contents = new ArraySet<>();
+    Set<String> images = new ArraySet<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -33,29 +41,61 @@ public class Challenge_all_dialog extends androidx.fragment.app.DialogFragment {
         Button joinChallengeBtn = (Button) view.findViewById(R.id.btn_joinChallenge);
         joinChallengeBtn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-//                Challenge_my_fragment challengeMy = new Challenge_my_fragment();
-//                challengeMy.newInstance(getArguments().getString("title"), getArguments().getString("content"));
                 Intent intent = new Intent(getContext(), MainActivity.class);
                 //휴대폰 내부 저장
-                sharedPreferences = getActivity().getSharedPreferences(MyPreferences, 0);
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                if(!sharedPreferences.getString("title", null).equals(getArguments().getString("title"))){
-                    editor.remove("day");
-                    editor.remove("date");
-                    editor.commit();
+                prefs = getActivity().getSharedPreferences(MyPreferences, 0);
+                SharedPreferences.Editor editor = prefs.edit();
+                //title 비교
+                boolean result = compareTitle();
+                if(result == true){
+                    defineElements();
+                    titles.add(getArguments().getString("title"));
+                    contents.add(getArguments().getString("content"));
+                    images.add(String.valueOf(getArguments().getInt("image")));
+                    editor.putStringSet("title", titles);
+                    editor.putStringSet("content", contents);
+                    editor.putStringSet("image", images);
+                    editor.apply();
+                    Log.v("all_dialog", sharedPreferences.getAll().toString());
+
+                    //updayNumber display
+                    startActivity(intent);
+                    Toast toasting = Toast.makeText(getActivity().getApplicationContext(), "나의 챌린지에 추가 성공!", Toast.LENGTH_SHORT);
+                    toasting.show();
+                    dismiss();
+                }
+                else{
+                    Toast toasting = Toast.makeText(getActivity().getApplicationContext(), "이미 존재하는 챌린지입니다.", Toast.LENGTH_SHORT);
+                    toasting.show();
                 }
 
-                editor.putString("title", getArguments().getString("title"));
-                editor.putString("content", getArguments().getString("content"));
-                editor.putInt("image", getArguments().getInt("image"));
-                editor.apply();
-                //updayNumber display
-                startActivity(intent);
-                Toast toasting = Toast.makeText(getActivity().getApplicationContext(), "나의 챌린지에 추가 성공!", Toast.LENGTH_SHORT);
-                toasting.show();
-                dismiss();
             }
         });
         return view;
+    }
+
+    //저장된 값 가져오기
+    public void defineElements() {
+        prefs = getActivity().getSharedPreferences(MyPreferences, 0);
+        if (prefs.getStringSet("title", null) != null) {
+            Log.v("all", prefs.getAll().toString());
+            if (!prefs.getAll().isEmpty()) {
+                titles.addAll(prefs.getStringSet("title", null));
+                contents.addAll(prefs.getStringSet("content", null));
+                images.addAll(prefs.getStringSet("image", null));
+            }
+        }
+    }
+
+    public boolean compareTitle(){
+        prefs = getActivity().getSharedPreferences(MyPreferences, 0);
+        if (prefs.getStringSet("title", null) != null) {
+            Set<String> set = prefs.getStringSet("title", null);
+            ArrayList<String> list = new ArrayList<>(set);
+            for(String str : list){
+                if(str.equals(getArguments().getString("title"))){ return false; }
+            }
+        }
+        return true;
     }
 }
--- app/src/main/java/org/techtown/drawer/Challenge_my_fragment.java
+++ app/src/main/java/org/techtown/drawer/Challenge_my_fragment.java
@@ -2,6 +2,7 @@ package org.techtown.drawer;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,25 +17,21 @@ import org.techtown.drawer.Adapter.ChallengeItemClickListener;
 import org.techtown.drawer.VO.ChallengeData;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 public class Challenge_my_fragment extends Fragment {
 
     //variable
-    private String title;
-    private String content;
-    private int image;
+//    private String title;
+//    private String content;
+//    private int image;
+    private Set<String> titles;
+    private Set<String> contents;
+    private Set<String> images;
     private ArrayList<ChallengeData> items = new ArrayList<>();
     private ListView myListView;
     public static final String MyPREFERENCES = "MyPrefs";
 
-//    public static ExplainCampaign newInstance(String param1, String param2) {
-//        ExplainCampaign fragment = new ExplainCampaign();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,20 +50,26 @@ public class Challenge_my_fragment extends Fragment {
 
         //itemList
         SharedPreferences prefs = getActivity().getSharedPreferences(MyPREFERENCES, 0);
-        if(prefs.contains("title") != false){
-            title = prefs.getString("title", null);
-            content = prefs.getString("content", null);
-            image = prefs.getInt("image", 0);
-            items.add(new ChallengeData(title, content, image));
+        if(!prefs.getAll().isEmpty()){
+            titles = prefs.getStringSet("title", null);
+            contents = prefs.getStringSet("content", null);
+            images = prefs.getStringSet("image", null);
+            Log.v("my", prefs.getAll().toString());
+            if(titles != null){
+                ArrayList<String> titleList = new ArrayList<>(titles);
+                ArrayList<String> contentList = new ArrayList<>(contents);
+                ArrayList<String> imageList = new ArrayList<>(images);
+                for(int i = 0; i < titleList.size(); i++){
+                    items.add(new ChallengeData(titleList.get(i), contentList.get(i), Integer.parseInt(imageList.get(i))));
+                }
+            }
         }
-
 
         adapter.setOnItemClickListener(new ChallengeItemClickListener() {
             @Override
             public void onItemClick(ChallengeAdapter.ViewHolderPage holder, View view, int position) {
                 Bundle bundle = new Bundle();
                 bundle.putString("title", items.get(position).getTitle());
-                bundle.putString("content", items.get(position).getContent());
                 Challenge_my_dialog dialog = new Challenge_my_dialog();
                 dialog.setArguments(bundle);
                 dialog.show(getActivity().getSupportFragmentManager(), "dialog_my");
